c868c5e87bb0ee483485e8492b7464c07d687e5e
Aayush Gupta <guptaaayush432@gmail.com>
2023-01-18 18:56:57 +0530

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
-contract SimpleStorage{
-    uint256 public favoriteNumber;
+contract SimpleStorage {
+  uint256 public favoriteNumber;
 
-    function store(uint256 _newFavoriteNumber) public {
-        favoriteNumber = _newFavoriteNumber;
-    }
+  event storeNumber(
+    uint256 indexed oldNumber,
+    uint256 indexed newNumber,
+    uint addedNumber,
+    address sender
+  );
+
+  function store(uint256 _newFavoriteNumber) public {
+    emit storeNumber(
+      favoriteNumber,
+      _newFavoriteNumber,
+      favoriteNumber + _newFavoriteNumber,
+      msg.sender
+    );
+    favoriteNumber = _newFavoriteNumber;
+  }
 }
